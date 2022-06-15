dbcd377525e1274ac412d2feab888b59f283686c
S.R Dragonfang <tuyhongsonshl@gmail.com>
2022-06-15 10:32:48 +0700

    Initial Author Routes

---
+++ README.md
@@ -0,0 +1,3 @@
+# NPM
+
+npm i body-parser
--- server.js
+++ server.js
@@ -5,11 +5,11 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-//   const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //   const methodOverride = require('method-override')
 
 const indexRouter = require("./routes/index");
-//   const authorRouter = require('./routes/authors')
+const authorRouter = require("./routes/authors");
 //   const bookRouter = require('./routes/books')
 
 app.set("view engine", "ejs");
@@ -19,7 +19,8 @@ app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 //   app.use(methodOverride('_method'))
 app.use(express.static("public"));
-//   app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
@@ -28,7 +29,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
-//   app.use('/authors', authorRouter)
+app.use("/authors", authorRouter);
 //   app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000);
